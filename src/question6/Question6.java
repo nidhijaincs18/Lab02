86bd9abd7536ec14390ba8cab0342b2a2f83324e
Nidhi <nidhi.jain_cs18@gla.ac.in>
2019-08-19 02:57:27 -0700

    add condition

--- src/question6/Question6.java
+++ src/question6/Question6.java
@@ -6,7 +6,12 @@ public class Question6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         char alpha1 = scanner.nextLine().charAt(0);
-        char alpha1 = scanner.nextLine().charAt(0);
+        char alpha2 = scanner.nextLine().charAt(0);
+        if (alpha1 < alpha2) {
+            System.out.println(alpha1 + " " + alpha2);
+        } else {
+            System.out.println(alpha2 + " " + alpha1);
+        }
         scanner.close();
     }
 }
