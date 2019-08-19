0adb8278d513e7fda3e4470f3af8f1192aaf055f
Nidhi <nidhi.jain_cs18@gla.ac.in>
2019-08-19 02:23:23 -0700

    add input statements for number1 and number2

--- src/question3/Question3.java
+++ src/question3/Question3.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Question3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
     }
 }
