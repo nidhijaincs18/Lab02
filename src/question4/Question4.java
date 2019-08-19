9ccb90af2d11a8bcce07d2d8db2b8740f70cd003
Nidhi <nidhi.jain_cs18@gla.ac.in>
2019-08-19 02:32:49 -0700

    add Scanner Class

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -1,7 +1,11 @@
 package question4;
 
+import java.util.Scanner;
+
 public class Question4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
 
     }
 }
