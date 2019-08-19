bfde9cc681ee5fa08d77066d22e29018fa01d9c8
Nidhi <nidhi.jain_cs18@gla.ac.in>
2019-08-19 02:12:13 -0700

    add statements for variable declaration

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -1,4 +1,13 @@
 package question2;
 
+import java.util.Scanner;
+
 public class Question2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String value1 = scanner.nextLine().trim();
+        String value1 = scanner.nextLine().trim();
+        System.out.println("Welcome, " + userName + "!");
+        scanner.close();
+    }
 }
