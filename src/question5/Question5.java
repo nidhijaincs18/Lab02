09399d9dca9ce674b39a5449b6b21f2c8641bdbc
Nidhi <nidhi.jain_cs18@gla.ac.in>
2019-08-19 02:41:35 -0700

    add input statement for (number,int)

--- src/question5/Question5.java
+++ src/question5/Question5.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Question5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int number = scanner.nextInt();
 
     }
 }
