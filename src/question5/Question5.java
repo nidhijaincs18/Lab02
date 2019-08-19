fbcc1074cac515712c54573811395ec1fb41eb00
Nidhi <nidhi.jain_cs18@gla.ac.in>
2019-08-19 02:42:24 -0700

    add if condition for even

--- src/question5/Question5.java
+++ src/question5/Question5.java
@@ -6,6 +6,9 @@ public class Question5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        }
 
     }
 }
