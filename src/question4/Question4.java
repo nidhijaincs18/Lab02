48ad61ece08b47b8621454c006bc81dedcad8cf0
Nidhi <nidhi.jain_cs18@gla.ac.in>
2019-08-19 02:35:39 -0700

    add if condition for positive

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class Question4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number > 0) {
+            System.out.println("Positive");
+        }
 
+    }
 
     }
 }
